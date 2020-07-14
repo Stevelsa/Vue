a416db4ee0b1c65ca5497774786bc7adbedba5dc
Stevelsa <13750555642@163.com>
2020-07-15 00:48:04 +0800

    add files on T490 to git

---
+++ vue-demo/src/components/SearchBar/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="brand">
+        <img src="@/assets/douban.jpg" alt class="logo">
+        <div class="search_input">
+            <el-input v-model="keyword" placeholder="please input" @keyup.enter.native="doSearch">
+            <el-button slot="append" icon="el-icon-search"  @click="doSearch"></el-button>
+            </el-input>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data(){
+        return{
+            keyword:this.$route.query.keyword ? this.$route.query.keyword:''
+        }
+    },
+    methods:{
+        doSearch(){
+            if(this.keyword.trim().length===0){
+                this.$message({
+                    showClose:true,
+                    message: '请输入要查询的内容',
+                    type:'warning'
+                })
+                return
+            }
+            this.$$route.push({
+                path:'/search',
+                query:{keyword:this.keyword,_random:Math.random()}
+            })
+        }
+    }
+}
+</script>
---
+++ vue-demo/src/views/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img src="@/assets/douban.jpg" width="25%" />
+    <router-link to="/film">
+      <h2>{{msg}}</h2>
+    </router-link>
+    <SearchBar></SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from '@/components/SearchBar/index.vue'
+
+export default {
+  name: "home",
+  componets:{
+    SearchBar
+  },
+  data() {
+    return {
+      msg: "豆瓣电影"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
